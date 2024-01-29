dd77fa214e8cd6fe9a7551bff9132d08bb806ce9
adhitiad <adhitiadwima@gmail.com>
2024-01-29 14:44:38 +0700

    Remove unused pembayaran route file.

--- src/utils/midtrans.ts
+++ src/utils/midtrans.ts
@@ -1,10 +1,1 @@
-import * as Midtrans from "midtrans-client";
-import "dotenv/config";
 
-const snap = new Midtrans.Snap({
-  isProduction: false,
-  serverKey: process.env.MIDTRANS_SERVER_KEY,
-  clientKey: process.env.MIDTRANS_CLIENT_KEY,
-});
-
-export default snap;
